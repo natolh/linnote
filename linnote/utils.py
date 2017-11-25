45d3feb256175a91e9c5f30fe3e1e052a88aa4d1
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-11-25 09:11:44 +0100

    Drop statistic function (move to composer)

--- linnote/utils.py
+++ linnote/utils.py
@@ -9,26 +9,13 @@ License: Mozilla Public License, see 'LICENSE.txt' for details.
 """
 
 from io import StringIO
-from statistics import mean as get_mean, median as get_median
-from jinja2 import Environment, PackageLoader
 from matplotlib import pyplot as plot
-from linnote.configuration import CHARSET
 
 
-def make_stats(sample):
-    """Basic statistics about an assessment's marks."""
-    # Return statistics.
-    return {
-        "size": len(sample),
-        "maximum": max(sample) if sample else 0,
-        "minimum": min(sample) if sample else 0,
-        "mean": get_mean(sample) if sample else 0,
-        "median": get_median(sample) if sample else 0
-    }
-
-
-def make_histogram(sample, coefficient):
+def histogram(assessment, group):
     """Distribution of marks to an assessment."""
+    sample = [mark for mark in assessment.results if mark.student in group]
+    coefficient = assessment.coefficient
     document = StringIO()
     plot.figure(figsize=(6, 4))
     plot.hist(x=sample, bins=coefficient, range=(0, coefficient),
@@ -37,20 +24,3 @@ def make_histogram(sample, coefficient):
     plot.savefig(document, format="svg")
     document.seek(0)
     return "\n".join(document.readlines()[5:-1])
-
-
-def render_template(template, **kwargs):
-    """
-    Load a template and render it.
-
-    - template: A string. Name of the template.
-    - kwargs:   Dictionnary. Arguments to fill the template.
-
-    Return: An HTML document.
-    """
-    # Load Jinja2 environnement and load the template.
-    env = Environment(loader=PackageLoader("linnote"))
-    template = env.get_template(template)
-
-    # Render, encode and return the template.
-    return template.render(**kwargs).encode(CHARSET)
