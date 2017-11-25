b9875795d986630ece33ea1c784a845630f56673
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-11-25 09:31:43 +0100

    Add histogram coposer

--- linnote/report/composer.py
+++ linnote/report/composer.py
@@ -8,9 +8,10 @@ Author: Anatole Hanniet, Tutorat Santé Lyon Sud (2014-2017).
 License: Mozilla Public License, see 'LICENSE.txt' for details.
 """
 
-
+from io import StringIO
 from operator import attrgetter
 from statistics import mean, median
+from matplotlib import pyplot
 
 value = attrgetter('value')
 
@@ -24,3 +25,20 @@ def statistics(assessment, group):
         "mean": mean(marks) if marks else 0,
         "median": median(marks) if marks else 0
     }
+
+
+def histogram(assessment, group):
+    document = StringIO()
+    coefficient = assessment.coefficient
+    marks = [value(m) for m in assessment.results if m.student in group]
+
+    pyplot.figure(figsize=(6, 4))
+    pyplot.hist(marks, bins=coefficient, range=(0, coefficient),
+                color=(0.80, 0.80, 0.80), histtype="stepfilled")
+    pyplot.title("Répartition des notes")
+    pyplot.savefig(document, format="svg")
+    document.seek(0)
+    return "\n".join(document.readlines()[5:-1])
+
+def ranking(assessment, group):
+    pass
