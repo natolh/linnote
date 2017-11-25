12d923c50524219111da25977eea21bcd19ffc50
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-11-25 08:30:12 +0100

    Create the composers report.composer module

---
+++ linnote/report/composer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Implement composers for creating reports.
+
+Author: Anatole Hanniet, Tutorat Santé Lyon Sud (2014-2017).
+License: Mozilla Public License, see 'LICENSE.txt' for details.
+"""
+
+from functools import wraps
+from operator import attrgetter
+
+def ranking(assessment, group):
+
+    return Ranking(marks, )
