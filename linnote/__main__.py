c01eac072cd110ac8c2a149b075be07dee933ee2
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-11-26 18:50:38 +0100

    Rename 'files' to 'RESULTS'

--- linnote/__main__.py
+++ linnote/__main__.py
@@ -6,7 +6,7 @@ from linnote.client import ranking
 from linnote.student import Group
 
 
-files = list(APP_DIR.joinpath('results').glob('*.xlsx'))
+RESULTS = list(APP_DIR.joinpath('results').glob('*.xlsx'))
 GROUPS = list(Group.load(file, file.stem) for file in Group.find())
 
-ranking(files, GROUPS)
+ranking(RESULTS, GROUPS)
