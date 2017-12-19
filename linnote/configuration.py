5834a4fc17adba31f5f38da0d54e77b058be1698
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2017-12-19 16:50:33 +0100

    Remove setlocale logic from the application

--- linnote/configuration.py
+++ linnote/configuration.py
@@ -7,8 +7,3 @@ Configuration.
 Author: Anatole Hanniet, Tutorat Santé Lyon Sud (2014-2017).
 License: Mozilla Public License, see 'LICENSE.txt' for details.
 """
-
-from platform import system as plateform
-
-
-LOCALE = 'French_France.1252' if plateform() == 'Windows' else 'fr_FR.UTF-8'
