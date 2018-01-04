6b5fa62291ba22a2c9e1d9889babce75987c3e60
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2018-01-04 19:16:15 +0100

    Add 'load' function in 'linnote.configuration'
    The 'load' function load a configuration file (INI-like) in the application.

--- linnote/configuration.py
+++ linnote/configuration.py
@@ -7,3 +7,23 @@ Configuration.
 Author: Anatole Hanniet, Tutorat Santé Lyon Sud (2014-2017).
 License: Mozilla Public License, see 'LICENSE.txt' for details.
 """
+
+from configparser import ConfigParser
+from linnote import APP_DIR
+
+
+DEFAULT_CONFIG = APP_DIR.joinpath('configuration.ini')
+
+
+def load(configfile=DEFAULT_CONFIG):
+    """
+    Load configuration from INI file.
+
+    - configfile:   A pathlike object. Path to the configuration file default to
+                    the file named 'configuration.ini' in the app directory.
+
+    Return: A config object.
+    """
+    configuration = ConfigParser()
+    configuration.read(configfile)
+    return configuration
