c7f6a0b3ec819718cfdee2fbd3de0ff5dd719391
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2018-01-10 15:17:35 +0100

    Add ‘assessment’ controller logic

--- linnote/client/__init__.py
+++ linnote/client/__init__.py
@@ -9,6 +9,39 @@ License: Mozilla Public License, see 'LICENSE.txt' for details.
 """
 
 from flask import Flask
+from flask import redirect, render_template, request
+from linnote import APP_DIR
+from linnote.client.forms import AssessmentForm
 
 
 APP = Flask('linnote')
+
+@APP.route('/')
+@APP.route('/index')
+@APP.route('/home')
+def home():
+    """Home page."""
+    return redirect('assessments', code=303)
+
+@APP.route('/assessments')
+def assessments():
+    """List of assessments."""
+    return render_template('assessments.html', assessments=APP_DIR.joinpath('results').glob('*.xlsx'))
+
+@APP.route('/assessment', methods=['GET', 'POST'])
+def assessment():
+    """An assessment."""
+    form = AssessmentForm(request.form)
+
+    if request.method == 'POST':
+        results = request.files['results']
+        extension = results.filename.rsplit('.', 1)[1]
+        results.save(str(APP_DIR.joinpath('results', form.title.data).with_suffix('.' + extension)))
+        return redirect('assessments', code=303)
+
+    return render_template('assessment.html', form=form)
+
+@APP.route('/reports')
+def reports():
+    """List of reports."""
+    return render_template('reports.html', reports=APP_DIR.joinpath('rankings').glob('*.html'))
--- linnote/client/forms.py
+++ linnote/client/forms.py
@@ -7,3 +7,14 @@ Forms for the client.
 Author: Anatole Hanniet, Tutorat Santé Lyon Sud (2014-2017).
 License: Mozilla Public License, see 'LICENSE.txt' for details.
 """
+
+from wtforms.form import Form
+from wtforms.fields import StringField, FileField, DecimalField, IntegerField
+
+
+class AssessmentForm(Form):
+    title = StringField('Libellé')
+    results = FileField('Notes')
+    scale = DecimalField('Barème', places=3)
+    coefficient = DecimalField('Coefficient', places=3, default=1)
+    precision = IntegerField('Précision', default=3)
