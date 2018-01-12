a5dc362606f19eff084b4a3811a2183a02eea99a
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2018-01-12 22:01:58 +0100

    Add module documentation for ‘__main__’

--- linnote/__main__.py
+++ linnote/__main__.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+u"""
+Main script for linnote.
+
+Author: Anatole Hanniet, Tutorat Santé Lyon Sud (2014-2017).
+License: Mozilla Public License, see 'LICENSE.txt' for details.
+"""
+
 from linnote.client import APP
+
 
 APP.run(host='0.0.0.0')
