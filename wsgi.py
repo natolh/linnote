414a010569ed7d142477a0ef33cef5230207cba8
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2018-02-02 14:36:52 +0100

    Implement blueprint registration

--- wsgi.py
+++ wsgi.py
@@ -9,6 +9,9 @@ License: Mozilla Public License, see 'LICENSE.txt' for details.
 """
 
 from linnote.client import create_app
+from linnote.client import api, auth, admin
 
 
-APPLICATION = create_app('linnote')
+BLUEPRINTS = [api.API, auth.AUTH, admin.ADMIN]
+APPLICATION = create_app('linnote', config_path='config.ini',
+                         blueprints=BLUEPRINTS)
