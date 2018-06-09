8949fb79a8cf6f6fdb7a3475de44ac530107a57b
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2018-06-09 17:43:10 +0200

    Fix issues

--- linnote/client/ressources/public/site/scripts/master.js
+++ linnote/client/ressources/public/site/scripts/master.js
@@ -1,19 +1,19 @@
 function xhr(METHOD, URL) {
     let req = new XMLHttpRequest();
-    req.responseType = "json"
+    req.responseType = "json";
     req.onreadystatechange = function() {
         if(req.readyState === 4 && req.status === 200) {
             window.location.replace(req.response.redirect);
-        }
+        };
     };
-    req.open(METHOD, URL, true);
+    req.open(METHOD, ENDPOINT, true);
     req.send();
 }
 
-var xhrButtons = document.querySelectorAll('.xhr')
+var xhrButtons = document.querySelectorAll(".xhr");
 for (var index = 0; index < xhrButtons.length; index++) {
     let button = xhrButtons[index];
-    let action = button.getAttribute('data-action')
-    let method = button.getAttribute('data-method')
-    button.addEventListener('click', function() {xhr(method, action)})
-}
+    let endpoint = button.getAttribute("data-action");
+    let method = button.getAttribute("data-method");
+    button.addEventListener("click", function() {xhr(method, endpoint)}),;
+};
