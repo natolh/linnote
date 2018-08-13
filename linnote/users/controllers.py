3c3c4dfa42c880d356117df3e1dae861ac10456d
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2018-08-13 10:42:45 +0200

    Add documentation to users.controllers

--- linnote/users/controllers.py
+++ linnote/users/controllers.py
@@ -8,6 +8,7 @@ Author: Anatole Hanniet, 2016-2018.
 License: Mozilla Public License, see 'LICENSE.txt' for details.
 """
 
+from typing import List
 from flask import redirect, render_template, request, url_for
 from flask.views import MethodView
 from flask_login import login_required
@@ -18,54 +19,61 @@ from .logic import load_group
 
 
 class GroupsController(MethodView):
-    """Controller for managing user groups collection."""
+    """Controls the view of users groups."""
 
     decorators = [login_required]
     template = 'groups/groups.html'
 
     def get(self):
-        """Display the collection of user groups."""
+        """Display users groups."""
         groups = self.load()
         return self.render(groups=groups)
 
     @staticmethod
-    def load():
-        """Load groups."""
+    def load() -> List[Group]:
+        """Load groups from storage."""
         data = DATA()
         groups = data.query(Group).all()
         return groups
 
     @classmethod
     def render(cls, **kwargs):
-        """Render groups view."""
+        """Render the view."""
         return render_template(cls.template, **kwargs)
 
 
 class GroupBaseController(MethodView):
-    """Controller for managing a user group ressource."""
+    """Common methods for controllers of users group views."""
 
     decorators = [login_required]
+    template = ''
 
     @staticmethod
-    def load(identifier):
+    def load(identifier: int) -> Group:
+        """Load a group from storage."""
         data = DATA()
         group = data.query(Group).get(identifier)
         return group
 
     @classmethod
     def render(cls, **kwargs):
+        """Render a view."""
         return render_template(cls.template, **kwargs)
 
 
 class GroupCreationController(GroupBaseController):
+    """Controls the view for creating new users groups."""
 
     template = 'groups/creation.html'
 
     def get(self):
+        """Display a form to gather data needed to create a users group."""
         form = GroupCreationForm()
         return self.render(form=form)
 
-    def post(self):
+    @staticmethod
+    def post():
+        """Create a new users group using data of the form."""
         form = GroupCreationForm()
         data = DATA()
 
@@ -82,17 +90,18 @@ class GroupCreationController(GroupBaseController):
 
 
 class GroupSettingsController(GroupBaseController):
+    """Controls the view of group's settings."""
 
     template = 'groups/group/settings.html'
 
-    def get(self, identifier):
-        """Display a form for creating a new user group."""
+    def get(self, identifier: int):
+        """Display group's settings."""
         group = self.load(identifier)
         form = GroupForm(obj=group)
         return self.render(form=form, group=group)
 
-    def post(self, identifier):
-        """Create a new user group."""
+    def post(self, identifier: int):
+        """Modify the group's settings."""
         data = DATA()
         group = self.load(identifier)
         form = GroupForm()
@@ -102,57 +111,62 @@ class GroupSettingsController(GroupBaseController):
 
 
 class GroupMembersController(GroupBaseController):
+    """Controls the view of group's members."""
 
     template = 'groups/group/members.html'
 
     def get(self, identifier):
+        """Display groups's members."""
         group = self.load(identifier)
         return self.render(group=group)
 
-    def post(self, identifier):
-        pass
-
 
 class UsersController(MethodView):
-    """Controller for managing users collection."""
+    """Controls the view of users."""
 
     decorators = [login_required]
     template = 'users/users.html'
 
     def get(self):
-        """Display the collection of users."""
+        """Display users."""
         users = self.load()
         return self.render(users=users)
 
     @staticmethod
-    def load():
+    def load() -> List[User]:
+        """Load users (only administrators) from storage."""
         data = DATA()
-        users = data.query(User).join(Profile).filter(Profile.role == 'administrator').all()
-        return users
+        users = data.query(User).join(Profile)
+        administrators = users.filter(Profile.role == 'administrator').all()
+        return administrators
 
     @classmethod
     def render(cls, **kwargs):
+        """Render the view."""
         return render_template(cls.template, **kwargs)
 
 
 class UserBaseController(MethodView):
-    """Controller for managing a user ressource."""
+    """Common methods for controllers of user's view."""
 
     decorators = [login_required]
     template = 'users/user.html'
 
     @staticmethod
-    def load(identifier=None):
+    def load(identifier: int) -> User:
+        """Load a user from storage."""
         data = DATA()
         user = data.query(User).get(identifier)
         return user
 
     @classmethod
     def render(cls, **kwargs):
+        """Render a view."""
         return render_template(cls.template, **kwargs)
 
 
 class UserCreationController(UserBaseController):
+    """Controls the view for creating a user."""
 
     template = 'users/creation.html'
 
@@ -161,7 +175,8 @@ class UserCreationController(UserBaseController):
         form = UserForm()
         return self.render(form=form, user=None)
 
-    def post(self):
+    @staticmethod
+    def post():
         """Create a new user."""
         form = UserForm()
         data = DATA()
@@ -178,15 +193,18 @@ class UserCreationController(UserBaseController):
 
 
 class UserController(UserBaseController):
+    """Controls the view of a user."""
 
-    def get(self, identifier):
+    def get(self, identifier: int):
+        """View a user."""
         data = DATA()
         user = self.load(identifier)
         form = UserForm(obj=user)
         form.groups.choices = [(g.identifier, g.name) for g in data.query(Group).all()]
         return self.render(form=form, user=user)
 
-    def post(self, identifier):
+    def post(self, identifier: int):
+        """Update user's details."""
         data = DATA()
         form = UserForm()
         form.groups.choices = [(g.identifier, g.name) for g in data.query(Group).all()]
@@ -200,5 +218,4 @@ class UserController(UserBaseController):
 
         data.add(user)
         data.commit()
-
         return self.get(identifier=identifier)
