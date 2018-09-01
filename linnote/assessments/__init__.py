094eb9812f2da316ca8b0308d822fd6046fd6dd6
Anatole Hanniet <anatole.hanniet@etu.univ-lyon1.fr>
2018-09-01 08:22:10 +0200

    Rewrite assessments modules routing

--- linnote/assessments/__init__.py
+++ linnote/assessments/__init__.py
@@ -9,32 +9,33 @@ License: Mozilla Public License, see 'LICENSE.txt' for details.
 """
 
 from flask import Blueprint
-from .controllers import AssessmentsController, AssessmentCreationController
-from .controllers import AssessmentSettingsController, AssessmentResultsController
+from .controllers import AssessmentsController
+from .controllers import AssessmentCreationController
 from .controllers import MergeController
+from .controllers import AssessmentResultsController
 from .controllers import AssessmentRankingsController
+from .controllers import AssessmentSettingsController
 
 
-# Build controllers functions.
-LIST_VIEW = AssessmentsController.as_view('assessments')
-ASSESSMENT_CREATION = AssessmentCreationController.as_view('assessment_creation')
-ASSESSMENT = AssessmentResultsController.as_view('assessment')
-SETTINGS = AssessmentSettingsController.as_view('settings')
-RESULTS_VIEW = AssessmentResultsController.as_view('results')
-MERGER_VIEW = MergeController.as_view('merger')
-RANKINGS_VIEW = AssessmentRankingsController.as_view('rankings')
-
-
-# Register routes to controllers.
+# Create the module.
 BLUEPRINT = Blueprint('assessments', __name__)
 BLUEPRINT.url_prefix = '/assessments'
 BLUEPRINT.template_folder = 'templates'
 
+# Build views' controllers.
+ASSESSMENTS = AssessmentsController.as_view('assessments')
+CREATOR = AssessmentCreationController.as_view('assessment_creation')
+ASSESSMENT = AssessmentResultsController.as_view('assessment')
+SETTINGS = AssessmentSettingsController.as_view('settings')
+RESULTS = AssessmentResultsController.as_view('results')
+MERGER = MergeController.as_view('merger')
+RANKINGS = AssessmentRankingsController.as_view('rankings')
 
-BLUEPRINT.add_url_rule('', view_func=LIST_VIEW)
-BLUEPRINT.add_url_rule('/', view_func=ASSESSMENT_CREATION)
+# Register views' controllers routes.
+BLUEPRINT.add_url_rule('', view_func=ASSESSMENTS)
+BLUEPRINT.add_url_rule('/', view_func=CREATOR)
+BLUEPRINT.add_url_rule('/merge', view_func=MERGER)
 BLUEPRINT.add_url_rule('/<int:identifier>', view_func=ASSESSMENT)
+BLUEPRINT.add_url_rule('/<int:identifier>/results', view_func=RESULTS)
+BLUEPRINT.add_url_rule('/<int:identifier>/rankings', view_func=RANKINGS)
 BLUEPRINT.add_url_rule('/<int:identifier>/settings', view_func=SETTINGS)
-BLUEPRINT.add_url_rule('/<int:identifier>/results', view_func=RESULTS_VIEW)
-BLUEPRINT.add_url_rule('/merge', view_func=MERGER_VIEW)
-BLUEPRINT.add_url_rule('/<int:identifier>/rankings', view_func=RANKINGS_VIEW)
